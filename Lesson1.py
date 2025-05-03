106b3c6cdaeb9b95c08f628484130bfce059fa05
Aron7R <aronmassuud7@gmail.com>
2025-05-03 22:00:07 +0600

    Add files via upload

---
+++ Lesson1.py
@@ -0,0 +1,28 @@
+print("Hello World")
+print("Aron massuud")
+print("Dhaka")
+print("Class-6")
+
+# This is a Comment
+name = "Baron" # This is a Variable
+print(name)
+name = "Aron"
+print(name)
+Class = 6
+print(Class)
+#Today we will learn about Python
+
+Color= "Aron"
+Age= "13"
+Food= "Pizza"
+#print("My favourite color is "+Color+" and I am "+Age+" Old My favourite food is "+Food+")
+print("My favourite color is "+Color+" and I am "+Age+" Old My favourite food is "+Food+"")
+
+Name = "Aron"
+Age = 12
+print(f"My Name is {Name} and I'm {Age} Years Old")
+
+
+a = "1"
+b = "2"
+print(f"A Value is {a} and B Value is {b}")
