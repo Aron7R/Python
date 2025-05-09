a216be267c6de10df493a519b57983b0970d06f7
Aron7R <aronmassuud7@gmail.com>
2025-05-09 22:01:56 +0600

    Add files via upload

---
+++ MyTurtle.py
@@ -0,0 +1,26 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("Learning Turtle")
+screen.bgcolor("grey")
+
+pen = turtle.Turtle()
+pen.pensize(3)
+pen.color("cyan")
+
+
+
+#Right Angle triangle
+pen.left(90)
+pen.forward(100)
+pen.right(120)
+pen.forward(200)
+pen.right(150)
+pen.forward(180)
+
+
+
+
+
+
+turtle.done()
