9c5edae185b957188e2e9153cd074db1af40e02c
Aron7R <aronmassuud7@gmail.com>
2026-01-23 22:24:26 +0600

    Update 1.13.py

--- 1.13.py
+++ 1.13.py
@@ -18,3 +18,35 @@ else:
 
 
     engine.say(text)
+    engine.runAndWait()
+
+    #Speech to Text Recognize Spoken Language (English)
+    def speech_to_text():
+        recognizer=sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Please Speak Now In English...")
+            audio=recognizer.listen(source)
+
+            try:
+
+                print("Recognizing speech...")
+
+                text=recognizer.recognize_google(audio,language='en-US') #Use English for speech recognition
+                print(f"You said: {text}")
+
+                return text
+            except.sr.UnknownValueError:
+                print("Sorry, Could not understand the audio")
+
+            except sr.RequestError as e:
+                print(f"API Error:{e}")
+
+
+    #Translate Text Using Google Translate AI
+    def translate_text(text,forget_language='es'): #Default Target Language is Spanish(es)
+        
+        translator=Traslators()
+        translation=translator.translate(text,dest=target_Language)
+        print(f"Translated Text:{translation.text}")
+
+        return translation.text
